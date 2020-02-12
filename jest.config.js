8ec3485048c3b45aa8d79d9f6c2480ae82a7c9c1
Ran Yitzhaki <ranyitz@gmail.com>
2020-02-12 02:01:25 +0200

    configure retries to kill flakiness in ci

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,6 @@
 module.exports = {
   testEnvironment: "node",
-  testMatch: ["**/__tests__/**/*.test.[jt]s?(x)"]
+  testMatch: ["**/__tests__/**/*.test.[jt]s?(x)"],
+  testRunner: "jest-circus/runner",
+  setupFilesAfterEnv: ["./jest.setup.js"]
 };
