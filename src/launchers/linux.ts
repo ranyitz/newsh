0bd3e0e8c5e935b72ffedcd9f784c65d1f01f89e
Ronan Sandford <wighawag@gmail.com>
2020-04-23 19:33:50 +0100

    add support for some default terminal on linux (#21)

--- src/launchers/linux.ts
+++ src/launchers/linux.ts
@@ -1,9 +1,16 @@
 import execa from "execa";
 import { Launcher } from "./";
+import commandExists = require("command-exists");
+const commandExistsSync = commandExists.sync;
 
 export const linux: Launcher = (execFilePath, options) => {
+  const terminal = options.terminalApp
+    ? options.terminalApp
+    : ["gnome-terminal", "konsole", "xterm", "io.elementary.terminal"].find(
+        commandExistsSync
+      );
   try {
-    execa.sync(options.terminalApp!, ["-e", `sh ${execFilePath}`], {
+    execa.sync(terminal!, ["-e", `sh ${execFilePath}`], {
       detached: true
     });
   } catch (error) {
